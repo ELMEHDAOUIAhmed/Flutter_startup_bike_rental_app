fe6dcf8156569d18f9a3ef135558c086a51d1ed5
ELMEHDAOUIAhmed <elmehdaoui.ahmed77@gmail.com>
2023-04-03 15:08:38 +0100

    updated selected map and fixed function

--- lib/tests/map_screen_new.dart
+++ lib/tests/map_screen_new.dart
@@ -16,6 +16,8 @@ class MapScreenNew extends StatefulWidget {
 }
 
 class _MapScreenNewState extends State<MapScreenNew> {
+  String id;
+  String userDestStation = '';
 
   //Starting location
   static const _initialCameraPosition = CameraPosition(
@@ -36,62 +38,6 @@ class _MapScreenNewState extends State<MapScreenNew> {
   bool _nav_bar_Visible = false;
   String _selectedMarkerId = '';
 
-
-
-//    List<Marker> _markers;
-//   _MapScreenNewState() : _markers = _getMarkers();
-
-
-
-//  List<Marker> _getMarkers() {
-//     return [
-//       Marker(
-//           markerId: MarkerId('1'),
-//           position: LatLng(36.715957, 3.186346),
-//           icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
-//           infoWindow: InfoWindow(
-//             title: 'Parking Enseignants',
-//             snippet: 'Stock: 10',
-//           ),
-//           onTap: () {
-//             _selectedMarkerId = '1';
-//           }),
-//       Marker(
-//           markerId: MarkerId('2'),
-//           position: LatLng(36.717171, 3.183816),
-//           icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
-//           infoWindow: InfoWindow(
-//             title: 'Faculté De Mathematique',
-//             snippet: 'Stock: 5',
-//           ),
-//           onTap: () {
-//             _selectedMarkerId = '2';
-//           }),
-//       Marker(
-//           markerId: MarkerId('3'),
-//           position: LatLng(36.713483, 3.176465),
-//           icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
-//           infoWindow: InfoWindow(
-//             title: 'Project Initiative Club',
-//             snippet: 'Stock: 2',
-//           ),
-//           onTap: () {
-//             _selectedMarkerId = '3';
-//           }),
-//       Marker(
-//           markerId: MarkerId('4'),
-//           position: LatLng(36.713716, 3.180834),
-//           icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
-//           infoWindow: InfoWindow(
-//             title: 'Bibliothèque Universitaire',
-//             snippet: 'Stock: 21',
-//           ),
-//           onTap: () {
-//             _selectedMarkerId = '4';
-//           }),
-//     ];
-//   }
-
   void _stationNotSelected() {
     showDialog(
       //you need a statefull widget to show dialog
@@ -108,58 +54,11 @@ class _MapScreenNewState extends State<MapScreenNew> {
               SizedBox(width: 8.0),
               Flexible(
                 fit: FlexFit.loose,
-                child: Text(
-                    'Select a Station on the map first!\n \nHint: Hold on any station to Select it!',
+                child: Text('Select a Station first!',
                     style: TextStyle(fontSize: 16.0)),
               ),
             ],
           ),
-        );
-      },
-    );
-  }
-
-  void _confirmRide() {
-    showDialog(
-      //you need a statefull widget to show dialog
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(48),
-          ),
-          title: Row(
-            children: const [
-              //Icon(Icons.error, color: Colors.red),
-              Icon(Icons.notification_important, color: Colors.yellow),
-              SizedBox(width: 8.0),
-              Flexible(
-                fit: FlexFit.loose,
-                child: Text(
-                    'Are you sure you want to start your Ride ?. \nImportant: you cannot cancel your ride after clicking YES !',
-                    style: TextStyle(fontSize: 16.0)),
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                _userDest = null;
-              },
-              child: Text("NO"),
-            ),
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  _ride_Visible = !_ride_Visible;
-                  _nav_bar_Visible = !_nav_bar_Visible;
-                });
-                Navigator.of(context).pop();
-              },
-              child: Text("YES"),
-            ),
-          ],
         );
       },
     );
@@ -202,28 +101,18 @@ class _MapScreenNewState extends State<MapScreenNew> {
         snippet: 'Stock: 21',
       ),
     ),
+    Marker(
+      markerId: MarkerId('5'),
+      position: LatLng(36.709513, 3.181112),
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+      infoWindow: InfoWindow(
+        title: 'Nouveaux Blocs Salles TP',
+        snippet: 'Stock: 1',
+      ),
+    ),
   ];
 
   Future<Position> _determinePosition() async {
-    // Commented because i'm asking permission in main.dart
-    // bool serviceEnabled;
-    // LocationPermission permission;
-
-    // serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    // if (!serviceEnabled) return Future.error('Location services are disabled!');
-    // permission = await Geolocator.checkPermission();
-    // if (permission == LocationPermission.denied) {
-    //   permission = await Geolocator.requestPermission();
-
-    //   if (permission == LocationPermission.denied) {
-    //     return Future.error("Location permission denied ");
-    //   }
-    // }
-
-    // if (permission == LocationPermission.deniedForever) {
-    //   return Future.error("Location permission are permanentely denied.! ");
-    // }
-
     Position position = await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.best,
       timeLimit: const Duration(seconds: 10),
@@ -248,15 +137,16 @@ class _MapScreenNewState extends State<MapScreenNew> {
       body: Stack(
         alignment: Alignment.center,
         children: [
-          GoogleMap(
-            myLocationButtonEnabled: false,
-            zoomControlsEnabled: false,
-            initialCameraPosition: _initialCameraPosition,
-            onMapCreated: (controller) => _googleMapController = controller,
-            //place markers on the screen
-            markers: {if (_user != null) _user, ...Set.from(_markers)},
-            onLongPress: _setDestination,
-          ),
+            GoogleMap(
+              myLocationButtonEnabled: false,
+              zoomControlsEnabled: false,
+              initialCameraPosition: _initialCameraPosition,
+              onMapCreated: (controller) => _googleMapController = controller,
+              //place markers on the screen
+              markers: {if (_user != null) _user, ...Set.from(_markers)},
+              //onLongPress: _setDestination,
+            ),
+
 
           Visibility(
             visible: _ride_Visible,
@@ -287,13 +177,7 @@ class _MapScreenNewState extends State<MapScreenNew> {
                           onPressed: () {
                             //Start sending user location
                             //take a ride button
-                            //if (user selected station on map) _confirmRide , else
-
-                            if (_userDest != null) {
-                              _confirmRide();
-                            } else {
-                              _stationNotSelected();
-                            }
+                            stationSelection();
                           },
                           style: TextButton.styleFrom(
                             padding: EdgeInsets.zero,
@@ -344,6 +228,7 @@ class _MapScreenNewState extends State<MapScreenNew> {
             // rectangle277p8d (1:38)
             left: 0 * fem,
             top: 0 * fem,
+
             child: Align(
               child: SizedBox(
                 width: 414 * fem,
@@ -442,7 +327,7 @@ class _MapScreenNewState extends State<MapScreenNew> {
                         CameraUpdate.newCameraPosition(CameraPosition(
                             target: LatLng(
                                 positionuser.latitude, positionuser.longitude),
-                            zoom: 17.5))
+                            zoom: 17))
                         // ignore: avoid_print
                         );
                     setState(() {
@@ -476,20 +361,12 @@ class _MapScreenNewState extends State<MapScreenNew> {
     );
   }
 
-//WORKS LIKE A CHARM
-  void _setDestination(LatLng pos) async {
-    //before saving check if Lat Lang exist in list of markers go throught the list of markers
-    // still deosnt work but ..
-    if (true) {
-      // Save the lat and lng of the selected marker to the destination variable
-      _dest = Position(latitude: pos.latitude, longitude: pos.longitude);
-      _userDest = _dest;
-      _dest = null;
+  String _getId(int index) {
+    return _markers[index].markerId.value;
+  }
 
-      // For debug remove after!!
-      // print('Destination: $_dest');
-
-      //show that the succefully selected a station
+  void stationSelection() {
+    if (id == null) {
       showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -497,46 +374,110 @@ class _MapScreenNewState extends State<MapScreenNew> {
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(48),
             ),
-            title: Row(
-              children: const [
-                //Icon(Icons.error, color: Colors.red),
-                Icon(Icons.check_circle, color: Colors.green),
-                SizedBox(width: 8.0),
-                Flexible(
-                  fit: FlexFit.loose,
-                  child: Text(
-                      'Station successfully selected. Click on Take a ride. \n\nHint:Holding another Station will alter the selected one!!',
-                      style: TextStyle(fontSize: 16.0)),
-                ),
-              ],
+            title: Text('Stations'),
+            content: Container(
+              width: double.maxFinite,
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: _markers.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    leading: Icon(Icons.directions_bike),
+                    title: Text(_markers[index].infoWindow.title),
+                    trailing: Text(_markers[index].infoWindow.snippet),
+                    onTap: () {
+                      id = _getId(index);
+                      Navigator.pop(context);
+
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(48),
+                            ),
+                            title: Row(
+                              children: [
+                                //Icon(Icons.error, color: Colors.red),
+                                Icon(Icons.check_circle, color: Colors.green),
+                                SizedBox(width: 8.0),
+                                Flexible(
+                                  fit: FlexFit.loose,
+                                  child: Text(
+                                      'Station \n "' +
+                                          _markers[index].infoWindow.title +
+                                          '"\n successfully selected. \n\n Go to your Station and Follow instruction to unlock Bike.',
+                                      style: TextStyle(fontSize: 16.0)),
+                                ),
+                              ],
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                  _confirmRide();
+                                },
+                                child: Text("Continue"),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                  );
+                },
+              ),
             ),
           );
         },
       );
     } else {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(48),
-            ),
-            title: Row(
-              children: const [
-                //Icon(Icons.error, color: Colors.red),
-                Icon(Icons.error, color: Colors.red),
-                SizedBox(width: 8.0),
-                Flexible(
-                  fit: FlexFit.loose,
-                  child: Text(
-                      'Not a Valid Station ! \nTry Again  \n\n Hint : Click on a station before Holding',
-                      style: TextStyle(fontSize: 16.0)),
-                ),
-              ],
-            ),
-          );
-        },
-      );
+      // wont happend
     }
+  }
+
+  void _confirmRide() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(48),
+          ),
+          title: Row(
+            children: [
+              //Icon(Icons.error, color: Colors.red),
+              Icon(Icons.notification_important, color: Colors.yellow),
+              SizedBox(width: 8.0),
+              Flexible(
+                fit: FlexFit.loose,
+                child: Text(
+                    'Are you sure you want to start your Ride ?.\n Important : you cannot cancel your ride after clicking YES !',
+                    style: TextStyle(fontSize: 16.0)),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                id = null;
+              },
+              child: Text("NO"),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _ride_Visible = !_ride_Visible;
+                  _nav_bar_Visible = !_nav_bar_Visible;
+                });
+                Navigator.of(context).pop();
+              },
+              child: Text("YES"),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
--- lib/tests/nav_bar_new.dart
+++ lib/tests/nav_bar_new.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import '/screens/home_page.dart';
-import '/tests/home_page_new.dart';
 
 class NavBar extends StatefulWidget {
   @override
@@ -13,85 +11,71 @@ class _NavBarState extends State<NavBar> {
   List<double> _iconScales = [1.5, 1.0, 1.0];
 
   void _onItemTapped(int index) {
-    // after we disable tap
-    // nav bar in this case it used for better UX 
     setState(() {
       _selectedIndex = index;
       _iconScales = [1.0, 1.0, 1.0];
-      _iconScales[index] = 1.5; // set the selected icon scale to 1.5
+      _iconScales[index] = 1.5;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    double baseWidth = 414.0024414062;
-    double fem = MediaQuery.of(context).size.width / baseWidth; // fetch phone screen size
-    double ffem = fem * 0.97;
-    return Container(
-      // add shadow after
-
-      height: 160, // or any specific height you want
-      // width: 414, // or any specific width you want
-      child: Scaffold(
-        //body: Home_page_new(),
-        //Home_page(), // execute the page behind the nav bar which is maps
-        extendBody: true,
-        bottomNavigationBar: SizedBox(
-          height: 160,
-          child: ClipRRect(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(50),
-            ),
-            child: BottomNavigationBar(
-              backgroundColor: Colors.white, // nav bar color
-              type: BottomNavigationBarType.fixed,
-              items: [
-                BottomNavigationBarItem(
-                  icon: Transform.scale(
-                    scale: _iconScales[
-                        0], // set the scale value based on the selected state
-                    child: Image.asset(
-                          _selectedIndex == 0
-                          ? 'assets/page-1/images/location_selected.png'
-                          : 'assets/page-1/images/location_not_selected.png',
-                      width: 38,
-                      height: 38,
-                    ),
+    return Positioned(
+      left: 0,
+      right: 0,
+      bottom: 0,
+      child: SizedBox(
+        height: 160,
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(50),
+          ),
+          child: BottomNavigationBar(
+            backgroundColor: Colors.white,
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(
+                icon: Transform.scale(
+                  scale: _iconScales[0],
+                  child: Image.asset(
+                    _selectedIndex == 0
+                        ? 'assets/page-1/images/location_selected.png'
+                        : 'assets/page-1/images/location_not_selected.png',
+                    width: 38,
+                    height: 38,
                   ),
-                  label: '', // remove the label
                 ),
-                BottomNavigationBarItem(
-                  icon: Transform.scale(
-                    scale: _iconScales[
-                        1], // set the scale value based on the selected state
-                    child: Image.asset(
-                      _selectedIndex == 1
-                          ? 'assets/page-1/images/lock_selected.png'
-                          : 'assets/page-1/images/unlock_not_selected.png',
-                      width: 38,
-                      height: 38,
-                    ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Transform.scale(
+                  scale: _iconScales[1],
+                  child: Image.asset(
+                    _selectedIndex == 1
+                        ? 'assets/page-1/images/lock_selected.png'
+                        : 'assets/page-1/images/unlock_not_selected.png',
+                    width: 38,
+                    height: 38,
                   ),
-                  label: '', // remove the label
                 ),
-                BottomNavigationBarItem(
-                  icon: Transform.scale(
-                    scale: _iconScales[
-                        2], // set the scale value based on the selected state
-                    child: Image.asset(
-                      _selectedIndex == 2
-                          ? 'assets/page-1/images/toolkit_selected.png'
-                          : 'assets/page-1/images/toolkit_not_slected.png',
-                      width: 38,
-                      height: 38,
-                    ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Transform.scale(
+                  scale: _iconScales[2],
+                  child: Image.asset(
+                    _selectedIndex == 2
+                        ? 'assets/page-1/images/toolkit_selected.png'
+                        : 'assets/page-1/images/toolkit_not_slected.png',
+                    width: 38,
+                    height: 38,
                   ),
-                  label: '', // remove the label
                 ),
-              ],
-              currentIndex: _selectedIndex,
-              onTap: _onItemTapped,
-            ),
+                label: '',
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
           ),
         ),
       ),
